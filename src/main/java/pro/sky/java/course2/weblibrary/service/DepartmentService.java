455a7f0e224ae67764fa602c710b3639f56ead99
19kuz <19kuzminykh78@mail.ru>
2023-11-10 21:22:54 +0800

    ДЗ 2.11

---
+++ src/main/java/pro/sky/java/course2/weblibrary/service/DepartmentService.java
@@ -0,0 +1,37 @@
+package pro.sky.java.course2.weblibrary.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.java.course2.weblibrary.model.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+@Service
+public class DepartmentService {
+    private EmployeeService employeeService;
+    public DepartmentService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    public Employee minSalary (int department) {
+        return employeeService.list().stream()
+                .filter(employee -> employee.getDepartment() == department)
+                .min(Comparator.comparingInt(Employee::getSalary))
+                .orElse(null);
+    }
+    public  Employee maxSalary (int department) {
+        return employeeService.list().stream()
+                .filter(employee -> employee.getDepartment() == department)
+                .max(Comparator.comparingInt(Employee::getSalary))
+                .orElse(null);
+    }
+    public Map< Integer, List<Employee>> printNameByDepartment(){
+        return employeeService.list().stream()
+                .collect(Collectors.groupingBy(Employee ::getDepartment));
+    }
+    public List<Employee> getName(int department){
+        return employeeService.list().stream()
+                .filter(employee ->employee.getDepartment() == department)
+                .collect(Collectors.toList());
+    }
+}
